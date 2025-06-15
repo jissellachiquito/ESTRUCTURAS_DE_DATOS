40a268950a04c913d04a20290882e1e82e6c529e
jissellachiquito <jk.chiquitoq@uea.edu.ec>
2025-06-14 19:02:46 -0500

    metodo construstor

--- semana2/tarea_semana_3.cs
+++ semana2/tarea_semana_3.cs
@@ -7,7 +7,15 @@ public class Estudiante //creacion de clase estudiante
     private string direccion;
     private string[] telefonos;  // array para los tres teléfonos
 
-    
+    //generacion del constructor para inicializar los datos
+    public Estudiante(int id, string nombres, string apellidos, string direccion, string[] telefonos)
+    {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.telefonos = telefonos;
+    }
     
 
  }
